7b0955ba0a95b2e177e7888c25e11e151214b3b6
Huyhoang120904 <hoangbeo1124@gmail.com>
2026-01-10 10:42:40 +0700

    base prj

---
+++ common/src/main/java/com/hoanghocdev/common/dto/Test.java
@@ -0,0 +1,17 @@
+package com.hoanghocdev.common.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Test {
+
+    String id;
+    String name;
+    String price;
+
+}
